4d3cd4dd9f2ad55c45ad5625293129d7ae064ecf
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-12-01 19:53:02 -0800

    day1 b

--- src/org/devpush/com/aoc2025/day1/Day1.kt
+++ src/org/devpush/com/aoc2025/day1/Day1.kt
@@ -13,11 +13,11 @@ fun main(){
             if(it[0]=='L'){
                 startPoint = (startPoint - move)
                 startPoint = (startPoint % totalSize + totalSize) % totalSize
-                println("L $startPoint")
+                //println("L $startPoint")
             }
             else{
                 startPoint = (startPoint + move) % totalSize
-                println("R $startPoint")
+                //println("R $startPoint")
             }
             if(startPoint==0){
                 zeroCount++
@@ -28,6 +28,35 @@ fun main(){
 
     }
 
+    fun part2(): Int{
+        var startPoint = 50
+        var zeroCount = 0
+        val totalSize = 100
+        val input = readInput(false,1,true)
+        input.forEach {
+            val move = it.drop(1).toInt()
+            if (it[0] == 'L') {
+                val distToZero = if (startPoint == 0) totalSize else startPoint
+                if (move >= distToZero){
+                    zeroCount++
+                    val remaining = move - distToZero
+                    zeroCount += remaining / totalSize
+                    startPoint = (totalSize - (remaining % totalSize)) % totalSize
+                }
+                else {
+                    startPoint = if (startPoint == 0) totalSize - move else startPoint - move
+                }
+            } else {
+                val virtualStart = startPoint
+                val virtualEnd = virtualStart + move
+                zeroCount += (virtualEnd / totalSize) - (virtualStart / totalSize)
+                startPoint = virtualEnd % totalSize
+            }
+        }
+        return zeroCount
+    }
+
     println(part1())
+    println(part2())
 
 }
